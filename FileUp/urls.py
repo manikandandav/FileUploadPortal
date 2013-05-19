f85a142086e697a1083e952ff625324a1937786a
Viswa <viswa@viswa-Satellite-L850.(none)>
2013-05-19 19:02:32 +0530

    Register portal updated as well

--- FileUp/urls.py
+++ FileUp/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import patterns, include, url
-from FileUp.views import login, home
+from FileUp.views import login, home, register, logout
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+    url ( r'^$',login),
     url ( r'^login/$',login),
     url ( r'^home/$', home),
+    url ( r'^register/$', register),
+    url ( r'^logout/$', logout),
     # url(r'^admin/', include(admin.site.urls)),
 )
